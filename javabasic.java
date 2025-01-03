ae352d0626578ba9616f1f10bfef64038ccaca76
Aditya Mittal <mittaladitya274@gmail.com>
2025-01-03 10:40:28 +0530

    Basic Print statement

---
+++ javabasic.java
@@ -0,0 +1,10 @@
+public class javabasic{
+    public static void main(String argd[]){
+       
+        System.out.print("Hello World\n");
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.print("Hello \nWorld");
+       
+    }
+}
